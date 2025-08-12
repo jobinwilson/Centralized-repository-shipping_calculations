8dc8b0aa0dfd24ef9c8201def5ac9ea0b4b5efd2
jobinwilson <jobinwilson@gmail.com>
2025-08-12 09:15:25 +0530

    Revert "added cost offset of 1$"
    This reverts commit 528e1bb64eb99435bc00bf4b2b9a1c61e0f80320.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -13,8 +13,5 @@ tax_rate = 0.05
 # add tax calculation
 shipping_cost = shipping_cost + tax_rate*shipping_cost
 
-# add a constatnt offset of $1
-shipping_cost += 1
-
 # Display the result
 print(f"Total Shipping Cost: {shipping_cost} USD")
