528e1bb64eb99435bc00bf4b2b9a1c61e0f80320
jobinwilson <jobinwilson@gmail.com>
2025-08-12 09:13:02 +0530

    added cost offset of 1$

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -13,5 +13,8 @@ tax_rate = 0.05
 # add tax calculation
 shipping_cost = shipping_cost + tax_rate*shipping_cost
 
+# add a constatnt offset of $1
+shipping_cost += 1
+
 # Display the result
 print(f"Total Shipping Cost: {shipping_cost} USD")
